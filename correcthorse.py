1da1f309a6f5199d312b9f0766e97f1d4d0a7bed
Evan <evanmiller2112@gmail.com>
2018-06-17 17:38:35 -0400

    Update correcthorse.py
    added gui interface yay

--- correcthorse.py
+++ correcthorse.py
@@ -3,8 +3,9 @@
 # Import SystemRandom for secure cryptography
 #from import random
 from random import SystemRandom
+import tkinter as tk
 
-# Specify dictionary file
+#Specify dictionary file
 # This dictionary file was taken from the following GitHub repo: https://github.com/first20hours/google-10000-english
 dictionary="./wordlist"
 
@@ -28,16 +29,41 @@ def genPassword(dlist, pwlist):
 password = []
 password = genPassword(dictList, password)
 
-print(" ")
-print("- - - - - - - - -")
-print("Your secure password for the day is...")
-print("- - - - - - - - -")
-print(" ")
-for i in password:
-    print(i)
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
 
-print("- - - - - - - - -")
-print("We recommend adding some capitalzation somewhere, ")
-print(" and punctuation or special chars and a number.")
-print("Thank you for caring about security.")
-print("- - - - - - - - -")
+    def create_widgets(self):
+        self.pw0 = tk.Button(self)
+        self.pw0["text"] = password[0]
+        self.pw0.pack(side="top")
+        self.pw1 = tk.Button(self)
+        self.pw1["text"] = password[1]
+        self.pw1.pack(side="top")
+        self.pw2 = tk.Button(self)
+        self.pw2["text"] = password[2]
+        self.pw2.pack(side="top")
+        self.pw3 = tk.Button(self)
+        self.pw3["text"] = password[3]
+        self.pw3.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+                       
+        def regenPw():
+            password = []
+            password = genPassword(dictList, password)
+            self.pw0["text"] = password[0]
+            self.pw1["text"] = password[1]
+            self.pw2["text"] = password[2]
+            self.pw3["text"] = password[3]
+            
+        self.regen = tk.Button(self, text="REGEN PASSWORD", command=regenPw)
+        self.regen.pack(side="bottom")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
